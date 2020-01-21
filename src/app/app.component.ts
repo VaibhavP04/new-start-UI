1549b9809382cf910c1c8fe8701dde2dc8bb87f6
VaibhavP04 <vaibhav.prahallad@gmail.com>
2020-01-22 01:48:06 +0530

    7 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Validators } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit  {
+  date = new FormControl(new Date());
+  caseNo = new FormControl('', [Validators.required]);
+  safetyReport = new FormControl();
+  patientId = new FormControl('', [Validators.required]);
+  maxDate = new Date();
+  drugName = new FormControl('', [Validators.required, Validators.maxLength(255)]);
+  reaction = new FormControl('', [Validators.required]);
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+  reports = ['Patient', 'Physician'];
+  sources = ['Clinical Study', 'Spontaneous', 'Literature']
+
+
+  ngOnInit() {
+    this.filteredOptions = this.reaction.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppMaterialModule } from './app.material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppMaterialModule, BrowserAnimationsModule ],
+  imports:      [ BrowserModule, FormsModule, AppMaterialModule, BrowserAnimationsModule, ReactiveFormsModule, FlexLayoutModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
